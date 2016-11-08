001f513410dfaf6fa96c8e4e70ce17178cc8ce9c
naveraa <navera@email.arizona.edu>
2016-11-07 17:56:40 -0700

    Made AirCraftCarrier, Needs Fixing

---
+++ Project/src/ships/AirCraftCarrier.java
@@ -0,0 +1,9 @@
+package ships;
+
+public class AirCraftCarrier extends Ship {
+	private ArrayList<Plane> Planes = new Arraylist<Plane>;
+	
+	public boolean Exocet(){
+		//FIXME
+	}
+}
--- Project/src/ships/PTBoat.java
+++ Project/src/ships/PTBoat.java
@@ -2,6 +2,5 @@ package ships;
 import base.*;
 
 public class PTBoat extends Ship {
-
 }
  
