7b4eaea81f8270928ac7dd31a0c0ebee17ba8fa3
t_tow <t_tow@DESKTOP-88TEVA2.Home>
2016-11-28 16:35:44 -0700

    Basic Framework for the Enemy Class based on the UML Diagram we turned in

---
+++ Project/src/base/Oponent.java
@@ -0,0 +1,24 @@
+package base;
+
+import java.util.*;
+import ships.*;
+
+
+public class Oponent {
+	private ArrayList<Ship> Ships;
+	
+	
+	
+	
+	public void takeTurn(){
+		return;
+	}
+	
+	
+	public void chooseLocation(){return;}
+	public void setShips(){return;}
+	public int RNG(){return 0;}
+	
+	
+	
+}
